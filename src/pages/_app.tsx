a3dc52d8842f397b1480ca3a08fb23db1fd74b1d
maruxan <garavagliamauricio@gmail.com>
2022-08-21 02:33:02 -0300

    feat: trcp next api handler

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ export default withTRPC<AppRouter>({
   config({ ctx }) {
     const url = process.env.NEXT_PUBLIC_VERCEL_URL
       ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trcp';
+      : 'http://localhost:3000/api/trpc';
 
     const links = [
       loggerLink(),
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { trpc } from 'utils/trpc';
 
 const Home: NextPage = () => {
+  const { data, error, isLoading } = trpc.useQuery(['hello']);
+
   return (
-    <div className="bg-black h-screen w-full flex items-center justify-center">
-      <h1 className="text-gray-50 text-6xl">t3-blog</h1>
+    <div className="bg-black text-gray-50 h-screen w-full flex flex-col items-center justify-center">
+      <Head>
+        <title>t3-blog</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1 className="text-6xl">t3-blog</h1>
+      {isLoading && <div>loading...</div>}
+      {error && <p>{JSON.stringify(error)}</p>}
+      {data && <p>{JSON.stringify(data)}</p>}
     </div>
   );
 };
